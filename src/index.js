a366e2bf8cad5d3e2e6e8d2b095f8f2a2d61f492
Nila Homar <nilahomar@gmail.com>
2022-05-09 01:04:44 +0200

    functional components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'tachyons';
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
